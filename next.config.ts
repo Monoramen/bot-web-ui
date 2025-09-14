ccd1089665fd96dd5636dd7be23348aa53adcb7f
monoramen <monoramens@gmail.com>
2025-09-14 21:14:17 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
     // Предупреждение: это позволит сборке завершиться успешно, даже если есть ошибки TypeScript.
     ignoreBuildErrors: true,
   },
+    async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Перехватывать все запросы, начинающиеся с /api/
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // и перенаправлять их сюда
+      },
+    ];
+  },
 };
-
 export default nextConfig;
